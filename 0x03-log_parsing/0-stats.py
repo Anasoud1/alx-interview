9b5443d18cee996bc0e8c19745c2cc04076f2d43
Anasou1 <anasoudsassi@gmail.com>
2024-05-02 15:00:54 +0100

    updating 0-state.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,32 @@
 #!/usr/bin/python3
-
-""" script that reads stdin line by line and computes metrics """
-
+"""script that reads stdin line by line and computes metrics"""
 import sys
-
-
-def printStatus(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-# sourcery skip: use-contextlib-suppress
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
+import re
 
 count = 0
-size = 0
+file_size = 0
+dic = {'200': 0, '301': 0, '400': 0, '401': 0,
+       '403': 0, '404': 0, '405': 0, '500': 0}
+
+
+def print_status(dic, file_size):
+    """print file size information"""
+    print(f'File size: {file_size}')
+    for k, v in dic.items():
+        if v != 0:
+            print(f'{k}: {v}')
+
 
 try:
     for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
+            print_status(dic, file_size)
 
-        stlist = line.split()
         count += 1
+        file_size += int(line.split()[-1])
 
-        try:
-            size += int(stlist[-1])
-        except Exception:
-            pass
-
-        try:
-            if stlist[-2] in statusCodes:
-                statusCodes[stlist[-2]] += 1
-        except Exception:
-            pass
-    printStatus(statusCodes, size)
-
-
+        status_code = line.split()[-2]
+        if status_code in dic:
+            dic[status_code] += 1
 except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
+    print_status(dic, file_size)
