0fe5ecaee6abef85809096d4fdf846875cb28290
Anasou1 <anasoudsassi@gmail.com>
2024-05-02 14:48:08 +0100

    updating 0-stats.py file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,32 +3,33 @@
 import sys
 import re
 
-
 count = 0
 file_size = 0
-dic = {'count_200': 0, 'count_301': 0, 'count_400': 0, 'count_401': 0,
-       'count_403': 0, 'count_404': 0, 'count_405': 0, 'count_500': 0}
+dic = {'200': 0, '301': 0, '400': 0, '401': 0,
+       '403': 0, '404': 0, '405': 0, '500': 0}
+
+
+def print_status(dic, file_size):
+    """print file size information"""
+    print(f'File size: {file_size}')
+    for k, v in dic.items():
+        if v != 0:
+            print(f'{k}: {v}')
+
 
 try:
     for line in sys.stdin:
         pattern = (r'^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} - \[\d{4}-\d{2}-\d{2} '
                    r'\d{2}:\d{2}:\d{2}.\d{6}\] "GET \/projects\/260 HTTP\/1.1"'
                    r' \d{1,3} \d+$')
-        count += 1
-        size = int(line.split()[-1])
-        status_code = line.split()[-2]
         if re.match(pattern, line):
-            if count % 10 == 0:
-                print(f'File size: {file_size}')
-                for k, v in dic.items():
-                    if v != 0:
-                        print(f'{k.split("_")[1]}: {v}')
-            for k, v in dic.items():
-                if k.split("_")[1] == status_code:
-                    dic[k] += 1
+            if count != 0 and count % 10 == 0:
+                print_status(dic, file_size)
+            count += 1
+            size = int(line.split()[-1])
+            status_code = line.split()[-2]
+            if status_code in dic:
+                dic[status_code] += 1
             file_size += size
 except KeyboardInterrupt:
-    print(f'File size: {file_size}')
-    for k, v in dic.items():
-        if v != 0:
-            print(f'{k.split("_")[1]}: {v}')
+    print_status(dic, file_size)
