e8e96b1a654f77414f9e3df0ff47b5c39048b67f
Anasoud1 <anasoudsassi.com>
2024-05-23 09:03:54 -0700

    adding readme and 0-starwars_characters.js files

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const mainUrl = 'https://swapi-api.alx-tools.com/api/films/';
+
+if (process.argv.length === 3) {
+  request.get(mainUrl, { json: true }, (err, res, body) => {
+    if (err) {
+      return console.error('Request failed:', err);
+    }
+    if (res.statusCode !== 200) {
+      return console.error('Error:', res.statusCode, body);
+    }
+    const characterUrls = body.results[process.argv[2]].characters;
+    const characterNames = characterUrls.map(
+      url => new Promise((resolve, reject) => {
+        request.get(url, { json: true }, (err, res, body) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(body.name);
+        });
+      }));
+    Promise.all(characterNames)
+      .then(names => console.log(names.join('\n')))
+      .catch(error => console.log(error));
+  });
+}
